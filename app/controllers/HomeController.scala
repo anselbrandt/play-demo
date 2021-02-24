afe960d3c15c79b38e692a45767fe8df82d82303
anselbrandt <ansel@anselbrandt.com>
2021-02-24 09:10:19 -0500

    Add gateway route

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -4,12 +4,18 @@ import javax.inject._
 import play.api._
 import play.api.mvc._
 import play.api.libs.json._
+import play.api.libs.ws._
+
+import scala.concurrent.{ExecutionContext}
 
 /** This controller creates an `Action` to handle HTTP requests to the
   * application's home page.
   */
 @Singleton
-class HomeController @Inject() (val controllerComponents: ControllerComponents)
+class HomeController @Inject() (
+    ws: WSClient,
+    val controllerComponents: ControllerComponents
+)(implicit ec: ExecutionContext)
     extends BaseController {
 
   /** Create an Action to render an HTML page.
@@ -26,5 +32,10 @@ class HomeController @Inject() (val controllerComponents: ControllerComponents)
   }
   def apiRoute() = Action { implicit request: Request[AnyContent] =>
     Ok(Json.obj("message" -> "hello"))
+  }
+  def gateway() = Action.async { implicit request: Request[AnyContent] =>
+    ws.url("https://api.kanye.rest").get().map { response =>
+      Ok(response.json)
+    }
   }
 }
